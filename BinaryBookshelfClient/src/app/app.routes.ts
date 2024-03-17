0b0b38c43d1291c1b282579a0dd2190f9744ba12
ivanespinoza518 <ivan.espinoza.7887@gmail.com>
2024-03-17 01:05:10 -0700

    Add components, navigation, and material for styling.

--- BinaryBookshelfClient/src/app/app.routes.ts
+++ BinaryBookshelfClient/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { BooksComponent } from './books/books.component';
+import { AuthorsComponent } from './authors/authors.component';
+import { CategoriesComponent } from './categories/categories.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent, pathMatch : 'full' },
+    { path: 'books', component: BooksComponent },
+    { path: 'authors', component: AuthorsComponent },
+    { path: 'categories', component: CategoriesComponent }
+];
